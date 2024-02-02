b8c7442f7c03e814f9eeaf6706a171ca6ac323ba
swaggp <mutebinuhu1@gmail.com>
2024-02-02 13:09:48 +0300

    core: few changes

--- components/Banner.js
+++ components/Banner.js
@@ -5,10 +5,10 @@ import LeadForm from './LeadForm';
 
 const Banner = () => {
     return (
-        <div className='md:flex justify-between space-x-4 relative'>
+        <div className='md:flex justify-between space-x-4 relative '>
             <div className=' w-full md:w-1/2'>
                 <Nav/>
-                <p className='leading-loose md:mt-6'>Experience double the luxury at <span className='bg-[#006B8D] text-white p-2 m-2'>Saadiyat Lagoon</span> and <br/> <span className='bg-[#006B8D] text-white p-2 m-2'>Yas Golf Collection!</span>
+                <p className='leading-loose md:mt-12 mt-6'>Experience double the luxury at <span className='bg-[#006B8D] text-white p-2 m-2'>Saadiyat Lagoon</span> and <br/> <span className='bg-[#006B8D] text-white p-2 '>Yas Golf Collection!</span>
                  Whether it's coastal elegance or golf-front opulence, your dream lifestyle awaits. Don't miss this unique opportunity to own a piece of these exclusive paradises. Click here to explore oia properties and elevate your living experience today</p>
                 <p className='text-4xl font-medium mt-4'>4-6 Bedroom Villas Available for sale, Hurry before they are all taken</p>
                <div className=''>
