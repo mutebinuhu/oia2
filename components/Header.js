e720c32ad8363765861a28c4c70f81eb3b543086
swaggp <mutebinuhu1@gmail.com>
2024-02-02 13:04:17 +0300

    chore: few changes

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='flex bg-gray-900 justify-center items-center space-x-4 p-2 '>
             <p className='text-white text-center text-xs'>Get 25% off discount this new year sale. Grab fast</p>
-            <Button className='bg-[#006B8D] text-xs' children="Talk to us"/>
+            <Button className='bg-[#006B8D] text-white text-xs' children="Talk to us"/>
         </div>
     );
 }
